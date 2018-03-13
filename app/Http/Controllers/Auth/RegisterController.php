c8d1c096bde5a0eeaa29bdc1c7fa9a96f5df9c1b
Wagenpark RocIT <rick.smallenbroek@gmail.com>
2018-03-13 11:29:30 +0100

    fix

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,7 +65,6 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'user_role' => 1,
             'password' => bcrypt($data['password']),
         ]);
     }
