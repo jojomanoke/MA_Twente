4a4cd508331322aaa9d9ddf5bbe5553c33c4b037
Wagenpark RocIT <rick.smallenbroek@gmail.com>
2018-03-20 09:47:40 +0100

    blades

---
+++ app/Http/Controllers/ErrorController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ErrorController extends Controller
+{
+    Public function overview(){
+
+    return view("/overview");
+    }
+
+    public function create(){
+
+    return view("/create");
+    }
+
+    public function edit(){
+
+    return view("/edit");
+    }
+}
