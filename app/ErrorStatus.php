1e652ef6988ed59c5ef7c6d6cb53db33e96931d0
jojomanoke <sylvanoverkuyl@hotmail.com>
2018-04-23 17:24:03 +0200

    v3

--- app/ErrorStatus.php
+++ app/ErrorStatus.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ErrorStatus extends Model
 {
     protected $table = 'errors_status';
-
+    public $timestamps = false;
     public function errors(){
         return $this->belongsToMany(Error::class);
     }
